0b7601907b87050e3d7f7c8d5445e7123a34101b
Mayank Rawat <thing.mk0098@gmail.com>
2026-02-09 13:48:16 +0530

    Update Navbar.jsx

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,18 +1,28 @@
-import './Navbar.css'
+import { useState } from "react";
+import "./Navbar.css";
+
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <>
-      <div className='nav'>
-        <div className="nav-logo">   EV - Olvision </div>
-        <ul className="nav-menu">
-          <li>Home</li>
-          <li>Explore</li>
-          <li>About</li>
-          <li className='nav-contact'>Contact</li>
-        </ul>
+    <div className="nav">
+      <div className="nav-logo">EV - Olvision</div>
+
+      <ul className={`nav-menu ${menuOpen ? "active" : ""}`}>
+        <li>Home</li>
+        <li>Explore</li>
+        <li>About</li>
+        <li className="nav-contact">Contact</li>
+      </ul>
+
+      {/* Hamburger Menu */}
+      <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
